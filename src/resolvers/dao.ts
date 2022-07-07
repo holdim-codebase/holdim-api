149c6f6a73b9c344a5ba218b3a78a6f03afeb10a
Zhanybek Sadvakassov <tbdttb@gmail.com>
2022-07-07 17:08:27 +0200

    ✨ User can follow/unfollow DAO

--- src/resolvers/dao.ts
+++ src/resolvers/dao.ts
@@ -3,6 +3,12 @@ import { repositories } from '../repositories'
 import { getTokenInfo } from '../repositories/token'
 import { ZerionNamespaces } from '../services/zerion/types'
 
+export const daoPersonalizedDataResolver: Resolvers['DaoPersonalizedData'] = {
+  followed: ({ id: daoId }, args, ctx) => repositories.userDaoFollow.findUnique({
+    where: { daoId_userId: { daoId: Number(daoId), userId: ctx.user.uid } },
+  }).then(Boolean),
+}
+
 export const DaoResolver: Resolvers['DAO'] = {
   id: ({ id }) => id.toString(),
   logo: ({ logo }) => logo,
@@ -19,11 +25,33 @@ export const DaoResolver: Resolvers['DAO'] = {
 
     return Object.values(assetInfos as ZerionNamespaces.AssetsNamespace.AssetInfo[]).map(assetInfo => assetInfo.asset)
   },
+
+  personalizedData: dao => dao,
 }
 
 export const daoQueryResolver: Resolvers['Query'] = {
   daos: (parent, { ids }) => {
     const whereQuery: Parameters<typeof repositories['dao']['findMany']>[0] = ids ? { where: { id: { in: ids.map(id => parseInt(id)) } } } : {}
     return repositories.dao.findMany(whereQuery)
+  },
+}
+
+export const daoMutationResolver: Resolvers['Mutation'] = {
+  followDao: async (parent, { daoId }, ctx) => {
+    await repositories.userDaoFollow.upsert({
+      select: { daoId: true },
+      where: { daoId_userId: { daoId: Number(daoId), userId: ctx.user.uid } },
+      create: { daoId: Number(daoId), userId: ctx.user.uid },
+      update: {},
+    })
+
+    return (await repositories.dao.findUnique({ where: { id: Number(daoId) } }))!
+  },
+  unfollowDao: async (parent, { daoId }, ctx) => {
+    await repositories.userDaoFollow.delete({
+      where: { daoId_userId: { daoId: Number(daoId), userId: ctx.user.uid } },
+    })
+
+    return (await repositories.dao.findUnique({ where: { id: Number(daoId) } }))!
   },
 }
--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,5 +1,5 @@
 import { Resolvers } from '../generated/graphql'
-import { DaoResolver, daoQueryResolver } from './dao'
+import { DaoResolver, daoQueryResolver, daoPersonalizedDataResolver } from './dao'
 import { proposalQueryResolvers, proposalResolver } from './proposal'
 import { tokenPersonlizedDataResolver, tokenResoler } from './token'
 import { userMutationResolvers, userQueryResolvers, userResoler, walletResolver } from './user'
@@ -8,6 +8,7 @@ export const resolvers: Resolvers = {
   User: userResoler,
   Proposal: proposalResolver,
   DAO: DaoResolver,
+  DaoPersonalizedData: daoPersonalizedDataResolver,
   Token: tokenResoler,
   TokenPersonalizedData: tokenPersonlizedDataResolver,
   Wallet: walletResolver,
