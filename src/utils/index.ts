48a27fed08245aa1c97b53f2d67c38b45b4f0143
Zhanybek Sadvakassov <tbdttb@gmail.com>
2022-07-21 13:41:12 +0200

    🐛 Fix token 0 quantity representation

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,5 +3,10 @@
  * @param decimals >0 interger
  * @returns formatted token representation
  */
-export const formatTokenWithDecimals = (tokenQuantity: string, decimals: number) =>
-  `${tokenQuantity.padStart(decimals, '0').substring(0, tokenQuantity.padStart(decimals, '0').length - decimals) || '0'}.${tokenQuantity.padStart(decimals, '0').substring(tokenQuantity.length - decimals)}`
+export const formatTokenWithDecimals = (tokenQuantity: string, decimals: number) => {
+  if (Number(tokenQuantity) === 0) {
+    return '0'
+  }
+
+  return `${tokenQuantity.padStart(decimals, '0').substring(0, tokenQuantity.padStart(decimals, '0').length - decimals) || '0'}.${tokenQuantity.padStart(decimals, '0').substring(tokenQuantity.length - decimals)}`
+}
